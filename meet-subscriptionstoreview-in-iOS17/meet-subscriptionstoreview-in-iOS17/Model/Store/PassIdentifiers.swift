ffaaeb47d8a7c2596226171677d328d871db3b07
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-11-06 22:18:33 +0800

    Add real group id

--- meet-subscriptionstoreview-in-iOS17/meet-subscriptionstoreview-in-iOS17/Model/Store/PassIdentifiers.swift
+++ meet-subscriptionstoreview-in-iOS17/meet-subscriptionstoreview-in-iOS17/Model/Store/PassIdentifiers.swift
@@ -19,6 +19,7 @@ extension EnvironmentValues {
     private enum PassIDsKey: EnvironmentKey {
         static var defaultValue = PassIdentifiers(
             group: "506F71A6",
+            //group: "21408633",
             monthly: "com.pass.monthly",
             quarterly: "com.pass.quarterly",
             yearly: "com.pass.yearly"
